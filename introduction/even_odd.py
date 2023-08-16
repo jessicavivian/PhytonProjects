a823ae1011a03bc651d5efa8762621dda407d6e1
Jéssica Vivian <jessicavivian.eng@gmail.com>
2023-08-16 16:57:14 +0000

    coding

---
+++ introduction/even_odd.py
@@ -0,0 +1,8 @@
+print("Give me an integer number:")
+number = int(input())
+
+if (number % 2) == 0:
+  print(str(number) + " is even!")
+  
+else:
+  print(str(number)+ " is odd!")
---
+++ introduction/min_max.py
@@ -0,0 +1,5 @@
+print("Give me a list of numbers:")
+
+numbers = int(input())
+
+print(numbers)
