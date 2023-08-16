b8f6047b54fe3432edddbdc5e9283cf666478787
Jéssica Vivian <jessicavivian.eng@gmail.com>
2023-08-16 00:00:07 +0000

    started introduction codes

---
+++ introduction/celsius_to_fahrenheit.py
@@ -0,0 +1,6 @@
+print("What is the weather temperature in Celsius (°C)?")
+celsius = float(input())
+
+fahrenheit = (celsius * (9/5)) + 32
+
+print("The temperature in Farenheit is " + str(fahrenheit) + " degrees")
